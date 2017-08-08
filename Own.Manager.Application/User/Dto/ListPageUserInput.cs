17e8ce3ecfad5a0d069509b9364383f48aa7a160
王西星 <932718952@qq.com>
2017-08-08 23:09:11 +0800

    用户管理页面修改

--- Own.Manager.Application/User/Dto/ListPageUserInput.cs
+++ Own.Manager.Application/User/Dto/ListPageUserInput.cs
@@ -13,6 +13,7 @@ namespace Own.Manager.User.Dto
         public string UserName { get; set; }
         public string LoginName { get; set; }
 
+
         //public PagedAndSortedResultRequestDto PageDto
         //{
         //    get
--- Own.Manager.Web/js/user/index.js
+++ Own.Manager.Web/js/user/index.js
@@ -1,14 +1,21 @@
 ﻿$(function () {
-    var $tb = $("#dg_user");
-    var td_id = "dg_user_toolbar";
-    var tb_url = "../User/List";
+    var $dgUser = $("#dgUser");
+    var dgUserToolbarId = "#dgUser_tb";
+    var dgUserUrl = "../User/List";
+
+    var $dlgAddUser = $("#dlgAddUser");
+    var $dlgAddUserForm = $("#dlgAddUser_form");
+    var dlgAddUserUrl;
+
     initDg();
+    initDlg();
+    initButton();
 
     function initDg() {
         var defaultOption = commonUtility.dg_default_option;
         var option = {
-            toolbar: td_id,
-            url: tb_url,
+            toolbar: dgUserToolbarId,
+            url: dgUserUrl,
             //loadFilter: function (data) {
             //    return data.result;
             //},
@@ -19,6 +26,79 @@
             ]]
         };
 
-        $tb.datagrid($.extend({}, defaultOption, option));
+        $dgUser.datagrid($.extend({}, defaultOption, option));
+    }
+    function initDlg() {
+        $dlgAddUser.dialog({
+            title: "New User",
+            buttons: "#dlgAddUser_buttons",
+            closed: true,
+            cache: false,
+            modal: true
+        });
+    }
+    function initButton() {
+        $("#dgUser_tb_btnNew").on("click", showUserAddDialog);
+        $("#dgUser_tb_btnEdit").on("click", showUserEditDialog)
+        $("#dlgAddUser_buttons_btnSave").on("click", SaveUser);
+        $("#dlgAddUser_buttons_btnClose").on("click", function () { $dlgAddUser.dialog("close"); });
+    }
+
+    function showUserAddDialog() {
+        $dlgAddUser.dialog('open').dialog('center').dialog('setTitle', 'New User');
+        $dlgAddUserForm.form('clear');
+        dlgAddUserUrl = '/Add';
+    }
+
+    function showUserEditDialog() {
+        var row = $dgUser.datagrid("getSelected");
+        if (row) {
+            $dlgAddUser.dialog('open').dialog('center').dialog('setTitle', 'Edit User');
+            $dlgAddUserForm.form('load', row);
+            url = '/Edit/' + row.id;
+        }
+        else {
+            $.messager.alert("提示", "请选择需要编辑的行!", "info");
+        }
+    }
+    function SaveUser() {
+        $dlgAddUserForm.form('submit', {
+            url: url,
+            onSubmit: function () {
+                return $(this).form('validate');
+            },
+            success: function (result) {
+                var result = eval('(' + result + ')');
+                if (result.errorMsg) {
+                    $.messager.show({
+                        title: 'Error',
+                        msg: result.errorMsg
+                    });
+                } else {
+                    $('#dlg').dialog('close');        // close the dialog
+                    $('#dg').datagrid('reload');    // reload the user data
+                }
+            }
+        });
+    }
+
+    function destroyUser() {
+        var row = $('#dg').datagrid('getSelected');
+        if (row) {
+            $.messager.confirm('Confirm', 'Are you sure you want to destroy this user?', function (r) {
+                if (r) {
+                    $.post('destroy_user.php', { id: row.id }, function (result) {
+                        if (result.success) {
+                            $('#dg').datagrid('reload');    // reload the user data
+                        } else {
+                            $.messager.show({    // show error message
+                                title: 'Error',
+                                msg: result.errorMsg
+                            });
+                        }
+                    }, 'json');
+                }
+            });
+        }
     }
 });
